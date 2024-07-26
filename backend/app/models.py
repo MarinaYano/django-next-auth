5be9eec740db5d513b316c7b07bcd6f169e80c4d
MarinaYano <myano2621@gmail.com>
2024-07-25 23:55:20 -0700

    add: create a model for posts

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from hashids import Hashids
 
-# Create your models here.
+class Post(models.Model):
+    uid = models.CharField('uid', max_length=30, unique=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='user', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post', verbose_name='image', null=True, blank=True)
+    title = models.CharField('title', max_length=255)
+    content = models.TextField('content')
+    created_at = models.DateTimeField('created_at', auto_now_add=True)
+    updated_at = models.DateTimeField('updated_at', auto_now=True)
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+
+    def __str__(self):
+        return self.title
+    
+@receiver(post_save, sender=Post)
+def generate_random_post_uid(sender, instance, created, **kwargs):
+    if created:
+        hashids = Hashids(salt='xRXSMT8XpzdUbDNM9qkv6raerwre3223', min_length=10)
+        instance.uid = hashids.encode(instance.id)
+        instance.save()
