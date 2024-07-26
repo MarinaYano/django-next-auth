4d79c3f8d8cb8e9f07dc87a90bf6b7979369727f
MarinaYano <myano2621@gmail.com>
2024-07-25 23:59:14 -0700

    add: create admin page

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from app.models import Post
 
-# Register your models here.
+class PostCustom(ModelAdmin):
+    list_display = ('uid', 'user', 'title', 'created_at', 'updated_at')
+    list_display_links = ('uid','user', 'title')
+    ordering = ('-created_at',)
+    readonly_fields = ('uid', 'created_at', 'updated_at')
+
+admin.site.register(Post, PostCustom)
