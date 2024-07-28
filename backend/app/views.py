4745695b74fc7320dd0f4a47cf28a3ea761850fd
MarinaYano <myano2621@gmail.com>
2024-07-27 23:46:39 -0700

    add: create views for posts

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListView(ListAPIView):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
+
+
+class PostDetailView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
+    lookup_field = 'uid'
+
+    def perform_create(self, serializer, **kwargs):
+        serializer.save(user=self.request.user)
