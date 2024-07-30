2c8f183b40e787a895d1463a766dd767052a6ac7
MarinaYano <myano2621@gmail.com>
2024-07-29 23:58:11 -0700

    add: create a server action file for posts

---
+++ frontend/actions/post/index.ts
@@ -0,0 +1,36 @@
+'use server';
+
+export const fetchAPI = async (url: string, options: RequestInit) => {
+  try {
+    const res = await fetch(`${process.env.API_URL}/${url}`, options)
+    
+    if(!res.ok) {
+      return { success: false, error: await res.json() }
+    }
+
+    const contentType = res.headers.get('Content-Type');
+    if (contentType && contentType.includes('application/json')) {
+      const data = await res.json();
+      return { success: true, data }
+    }
+
+    return { success: true }
+  } catch (error) {
+    console.log(error);
+    return { success: false }
+  }
+}
+
+export const getPostList = async () => {
+  const options: RequestInit = {
+    method: 'GET',
+    cache: 'no-cache',
+  }
+
+  const result = await fetchAPI('api/post-list/', options);
+
+  if(!result.success) {
+    console.log(result.error);
+    return { success: false, posts: [] }
+  }
+}
